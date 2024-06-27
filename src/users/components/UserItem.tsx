8c3b12efa5466b3b40dc7d8337f80274be3e0e29
dwidharmas <dwi.dharma97@gmail.com>
2024-06-27 16:44:01 +0800

    fix user to user type

--- src/users/components/UserItem.tsx
+++ src/users/components/UserItem.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { User } from "../interfaces/user.interface";
-import { Link } from "react-router-dom"
+import { UserType } from "../interfaces/user.interface.ts";
+import { Link } from "react-router-dom";
 
 import Avatar from "../../shared/components/UIElement/Avatar/Avatar.tsx";
 import Card from "../../shared/components/UIElement/Card/Card.tsx";
-import "./UserItem.css"
+import "./UserItem.css";
 
-const UserItems = (props: User) => {
-
+const UserItems = (props: UserType) => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
@@ -17,12 +16,14 @@ const UserItems = (props: User) => {
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
-            <h3>{props.places} {props.places === 1 ? 'place' : 'places' }</h3>
+            <h3>
+              {props.places} {props.places === 1 ? "place" : "places"}
+            </h3>
           </div>
         </Link>
       </Card>
     </li>
-  )
-}
+  );
+};
 
-export default UserItems
+export default UserItems;
