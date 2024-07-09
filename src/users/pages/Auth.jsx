96fb33b7e390784ab62d2bb2b0f5b3e2bbbdd88c
dwidharmas <dwi.dharma97@gmail.com>
2024-07-09 16:00:42 +0800

    day13-ch169

--- src/users/pages/Auth.jsx
+++ src/users/pages/Auth.jsx
@@ -63,8 +63,6 @@ const Auth = () => {
   const authSubmitHanlder = async (event) => {
     event.preventDefault();
 
-    console.log("form ", formState.inputs);
-
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
@@ -83,17 +81,15 @@ const Auth = () => {
       } catch (error) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
         const responseData = await sendRequest(
           "http://localhost:8000/api/users/signup",
           "POST",
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            "Content-Type": "application/json",
-          }
+          formData
         );
 
         auth.login(responseData.user.id);
