0650aa2c71462890a363698b78c57e056d01ec89
dwidharmas <dwi.dharma97@gmail.com>
2024-07-10 14:49:09 +0800

    day14-ch190

--- src/App.tsx
+++ src/App.tsx
@@ -15,22 +15,28 @@ import UpdatePlace from "./places/pages/UpdatePlace.tsx";
 
 import { AuthContext } from "./shared/context/auth-context.tsx";
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState<string | null>(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState<Date | null>(
+    null
+  );
   const [userId, setUserId] = useState<string | null>(null);
 
   const login = useCallback(
     (uid: string, token: string, expirationDate: Date) => {
       setToken(token);
       setUserId(uid);
-      const tokenExpirationData =
-        expirationDate || new Date(new Date().getTime() + 1000 * 60 + 60);
+      const tempTokenExpirationDate =
+        expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+      setTokenExpirationDate(tempTokenExpirationDate);
       localStorage.setItem(
         "userData",
         JSON.stringify({
           userId: uid,
           token: token,
-          expiration: tokenExpirationData.toISOString(),
+          expiration: tempTokenExpirationDate.toISOString(),
         })
       );
     },
@@ -40,6 +46,7 @@ const App = () => {
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
     localStorage.removeItem("userData");
   }, []);
 
@@ -57,6 +64,16 @@ const App = () => {
       );
     }
   }, [login]);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime: number =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   let routes;
 
